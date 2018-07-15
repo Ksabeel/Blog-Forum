548935001b234c4df7be082becce179b79a48940
Sabeel Abid <sabeelali13@gmail.com>
2018-07-16 02:37:06 +0500

    validation errors and old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,10 @@
                             <a class="nav-link" href="/threads">{{ __('All Threads') }}</a>
                         </li>
 
+                        <li class="nav-item">
+                            <a class="nav-link" href="/threads/create">{{ __('New Threads') }}</a>
+                        </li>
+
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 Channels <span class="caret"></span>
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,12 +13,44 @@
 
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text" name="title" class="form-control">
+                            <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title') }}" required autofocus>
+
+                            @if ($errors->has('title'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('title') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="channel_id">Choose a Channel</label>
+                            <select name="channel_id" id="channel_id" class="form-control{{ $errors->has('channel_id') ? ' is-invalid' : '' }}" required autofocus>
+                                    <option>Select Channel</option>
+                                @foreach (App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                        {{ $channel->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+
+                            @if ($errors->has('channel_id'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('channel_id') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group">
                             <label for="body">Body</label>
-                            <textarea name="body" id="body" rows="8" class="form-control"></textarea>
+                            <textarea name="body" id="body" rows="8" class="form-control{{ $errors->has('body') ? ' is-invalid' : '' }}" required autofocus>
+                                {{ old('body') }}
+                            </textarea>
+
+                            @if ($errors->has('body'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <button type="submit" class="btn btn-outline-primary">Publish</button>
