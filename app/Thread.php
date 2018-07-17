b2e3910084026e30eff3da676a73ab9fbdae7528
Sabeel Abid <sabeelali13@gmail.com>
2018-07-17 23:42:44 +0500

    global scopes and further query reduction

--- app/Thread.php
+++ app/Thread.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    /**
+     * Don't auto-apply mass assignment protection.
+     *
+     * @var array
+    */
     protected $guarded = [];
+
+    protected $with = ['creator', 'channel'];
     
     public static function boot()
     {
@@ -21,12 +28,10 @@ class Thread extends Model
     {
         return "/threads/{$this->channel->slug}/{$this->id}";
     }
-
+    
     public function replies()
     {
-    	return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+    	return $this->hasMany(Reply::class);
     }
 
     public function creator()
