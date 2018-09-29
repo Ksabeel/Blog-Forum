e63644d4738930265d80ad43073a50450f3d8667
Sabeel Abid <sabeelali13@gmail.com>
2018-09-30 00:01:17 +0500

    basic view tweaks

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,40 @@
+@forelse ($threads as $thread)
+    <div class="card mb-3">
+
+        <div class="card-header">
+            <div class="float-left">
+                <h5>
+                    Posted by: <a href="{{ route('profiles', $thread->creator) }}">
+                        {{ $thread->creator->name }}
+                    </a>
+                </h5>
+
+                <a href="{{ $thread->path() }}">
+
+
+                    @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+                        
+                        <strong>{{ $thread->title }}</strong>
+                    @else
+
+                        {{ $thread->title }}
+
+                    @endif
+                </a>
+            </div>
+
+            <div class="float-right">
+                <a href="{{ $thread->path() }}">
+                    {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
+                </a>
+            </div>
+        </div>
+
+        <div class="card-body">
+            {{ $thread->body }}
+        </div>
+
+    </div>
+@empty
+    <p class="text-center">There are no relevant results at this time.</p>
+@endforelse
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,40 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @forelse ($threads as $thread)
-                <div class="card mb-3">
-
-                    <div class="card-header">
-                        <div class="float-left">
-                            <a href="{{ $thread->path() }}">
-
-                                @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                    
-                                    <strong>{{ $thread->title }}</strong>
-                                @else
-
-                                    {{ $thread->title }}
-
-                                @endif
-                            </a>
-                        </div>
-
-                        <div class="float-right">
-                            <a href="{{ $thread->path() }}">
-                                {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="card-body">
-                        {{ $thread->body }}
-                    </div>
-
-                </div>
-            @empty
-                <p class="text-center">There are no relevant results at this time.</p>
-            @endforelse
+            @include ('threads._list')
+            
+            {{ $threads->links() }}
         </div>
+
     </div>
 </div>
 @endsection
